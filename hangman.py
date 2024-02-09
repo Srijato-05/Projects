cead4cbbf9c3bf6f8ae33450c83ccd0533cdb79f
Srijato Das <ssrijato.das@gmail.com>
2024-02-10 00:01:24 +0530

    Update hangman.py
    Corrected faulty logic in the code

--- hangman.py
+++ hangman.py
@@ -2,37 +2,44 @@ import random
 from colorama import Fore, Back, Style, init
 init(autoreset=True)
 
-words = ["aceola","aprium","banana","babaco","cherry","durian","feijoa","lemons","nutmeg","orange","papaya","tomato"]
+words = ["aceola", "aprium", "banana", "babaco", "cherry", "durian", "feijoa", "lemons", "nutmeg", "orange", "papaya", "tomato"]
 random_word = random.choice(words)
-guess_answer = ['_'] * len(random_word)
-incorrect_letter_position = []
-tries_left = 5
-print("Random Word: ", random_word)
 
 print("WORDLE GAME\nYou have 5 tries to guess the 6 letter word. Good Luck!")
-guess = (input("Guess the 6 letter word: ")).lower()
+tries_left = 5
+print(random_word)
+while tries_left > 0:
+    guess = input("Guess the 6 letter word: ").lower()
 
-while (tries_left > 0):
+    if len(guess) != len(random_word):
+        print("Invalid Guess. Try Again")
+        tries_left -= 1
+        print("Tries left: ", tries_left)
+        if tries_left == 0:
+            print("You Lose")
+            print("The word was: ", random_word)
+        continue
+
     if guess == random_word:
-        print(f"{Fore.GREEN}{Back.LIGHTGREEN_EX}{guess}{Fore.RESET}{Back.RESET}", end=" ")
-        print("You Win")
+        print(f"{Fore.GREEN}{Back.LIGHTGREEN_EX}{guess}{Fore.RESET}{Back.RESET}\nYou Win!")
         break
-    
-    for letter in range (len(guess)):
-        if len(guess) != len(random_word):
-            print("Invalid Guess. Try Again")
-            continue
+
+    feedback = ""
+    for letter in range(len(guess)):
         if guess[letter] == random_word[letter]:
-            print(f"{Fore.GREEN}{Back.LIGHTGREEN_EX}{guess[letter]}{Fore.RESET}{Back.RESET}", end=" ")
-            
-        elif guess[letter] != random_word[letter] and guess[letter] in random_word:
-            print(f"{Fore.YELLOW}{Back.LIGHTYELLOW_EX}{guess[letter]}{Fore.RESET}{Back.RESET}", end=" ")
-        else: 
-            print(f"{Fore.RED}{Back.LIGHTRED_EX}{guess[letter]}{Fore.RESET}{Back.RESET}", end=" ")
-            
-    print()
-    print("Try Again")
-    tries_left -= 1
-    guess = input("Guess the word: ")
+            feedback += f"{Fore.GREEN}{Back.LIGHTGREEN_EX}{guess[letter]}{Fore.RESET}{Back.RESET}"
+        elif guess[letter] in random_word:
+            feedback += f"{Fore.YELLOW}{Back.LIGHTYELLOW_EX}{guess[letter]}{Fore.RESET}{Back.RESET}"
+        else:
+            feedback += f"{Fore.RED}{Back.LIGHTRED_EX}{guess[letter]}{Fore.RESET}{Back.RESET}"
+
+    print(feedback)
+    
+    if guess != random_word:
+        tries_left -= 1
+        print("Try Again")
+        print("Tries left: ", tries_left)
+
     if tries_left == 0:
         print("You Lose")
+        print("The word was: ", random_word)
