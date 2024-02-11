67688da2ee31b4e55db31fd301fda818a872be8f
Srijato Das <ssrijato.das@gmail.com>
2024-02-11 22:19:17 +0530

    Added comments

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,24 +1,50 @@
+# Simple Number Guessing Game by Srijato Das
+
 import random 
 
-number = random.randint(1, 250)
-name=input("Enter your name: ")
-print("Hello, ",name)
+# Generate a random integer between 1 and 250
+number = random.rand(1, 250)
+
+# Get the user's name and greet them
+name = input("Enter your name: ")
+print("Hello, ", name)
+
+# Get the user's first guess
 guess = int(input("Enter an integer from 1 to 250: "))
-tries_left=10
-while number != "guess":
-    if tries_left==0:
-        print("You have no more tries left. The number was ",number)
+
+# Set the number of tries left to 10
+tries_left = 10
+
+# Loop until the user's guess is equal to the random number
+while number != guess:
+   
+    # Check if the user has no tries left
+    if tries_left == 0:
+        # Print a message and break the loop
+        print("You lost !!")
+        print("You have no more tries left. The number was ", number)
         break
+
+    # Check if the user's guess is less than the random number
     if guess < number:
+        # Print a message and decrement the number of tries left
         print("Guess is too Low")
-        tries_left-=1
+        tries_left -= 1
+        # Get the user's next guess
         guess = int(input("Enter an integer from 1 to 250: "))
+
+    # Check if the user's guess is greater than the random number
     elif guess > number:
+        # Print a message and decrement the number of tries left
         print("Guess is too High")
-        tries_left-=1
+        tries_left -= 1
+        # Get the user's next guess
         guess = int(input("Enter an integer from 1 to 250: "))
+
+    # Check if the user's guess is equal to the random number
     else:
-        tries_left-=1
+        # Print a message, decrement the number of tries left, and break the loop
+        tries_left -= 1
         print("Congratulations, ", name, "!! You guessed it!")
-        print("It took you ",10-tries_left," tries")
+        print("It took you ", 10 - tries_left, " tries")
         break
